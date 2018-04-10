5e23125bf79dbe4bb0386d81bec9901ce726687d
赵宝瑞 <zj0zbr@zbr.local>
2018-04-10 14:35:53 +0800

    add/test

---
+++ function/datetype.py
@@ -0,0 +1,19 @@
+print("========首字母大写击全大全小======")
+name = "adA mDdng"
+print(name.title()) #首字母大写
+print(name.lower());print(name.upper())#全小 全大
+
+print("=====多个变量======")
+name = "zbr"
+print(name)
+name= "zj"
+print(name)
+
+print("======合并字符串======")
+first_name = "zhao"
+last_name = "baorui"
+full_name = first_name + " " + last_name
+print(full_name)
+
+print("Hello, "+full_name.title() + "!")
+
